46c4fe2ad6af1fb91f3eee4b489806f89cb4e161
Hafzan Ahamed <hafzanahame99@gmail.com>
2026-02-13 14:55:49 +0530

    chore(project): update components path

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import Footer from "../../components/footer";
-import Header from "../../components/Header";
+import Footer from "../components/footer";
+import Header from "../components/Header";
 import "./globals.css";
 
 const geistSans = Geist({
